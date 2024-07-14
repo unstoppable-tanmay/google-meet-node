3a9cc165faafaa4f27b3e79e6e105994f2362cdc
unstoppable-tanmay <iamunstoppableguy@gmail.com>
2024-07-14 21:28:30 +0530

    fix: ip changes

--- src/lib/getIp.ts
+++ src/lib/getIp.ts
@@ -1,18 +1,29 @@
 const os = require("os");
 
+const ifaces = os.networkInterfaces();
+
 export const getIp = () => {
-  const networkInterfaces = os.networkInterfaces();
-  let ipv4Address = "";
-
-  for (const interfaceName in networkInterfaces) {
-    const interfaceInfo = networkInterfaces[interfaceName];
-    for (const alias of interfaceInfo) {
-      if (alias.family === "IPv4" && !alias.internal) {
-        ipv4Address = alias.address;
-        break;
+  let localIp = "127.0.0.1";
+  Object.keys(ifaces).forEach((ifname) => {
+    for (const iface of ifaces[ifname]) {
+      // Ignore IPv6 and 127.0.0.1
+      if (iface.family !== "IPv4" || iface.internal !== false) {
+        continue;
       }
+      // Set the local ip to the first IPv4 address found and exit the loop
+      localIp = iface.address;
+      return;
     }
-  }
+  });
+  console.log(localIp);
+  return localIp;
+};
 
-  return ipv4Address;
+export const returnIps = () => {
+  if (process.env.PUBLIC_IPS) {
+    return (JSON.parse(process.env.PUBLIC_IPS) as string[]).map((e) => ({
+      ip: "0.0.0.0",
+      announcedIp: e,
+    }));
+  } else return [{ ip: "0.0.0.0", announcedIp: getIp() }];
 };
