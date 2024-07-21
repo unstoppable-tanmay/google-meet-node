6ab327e3a3329dee7b4c771439f7e19be3769231
unstoppable-tanmay <iamunstoppableguy@gmail.com>
2024-07-21 21:16:47 +0530

    fix: ips trying to host

--- src/socket/media-events.ts
+++ src/socket/media-events.ts
@@ -15,7 +15,7 @@ import { Transport } from "mediasoup/node/lib/Transport";
 import { Consumer } from "mediasoup/node/lib/Consumer";
 import { Producer } from "mediasoup/node/lib/Producer";
 import { Router } from "mediasoup/node/lib/types";
-import { returnIps } from "../lib/getIp";
+import { getIp, returnIps } from "../lib/getIp";
 
 export const mediaEvents = (socket: Socket) => {
   // Client emits a request to create server side Transport
@@ -289,10 +289,10 @@ const createWebRtcTransport = async (router: Router) => {
       const webRtcTransport_options = {
         listenIps: [
           {
-            ip: "0.0.0.0",
-            announcedIp: process.env.HOST_PUBLIC_IP ?? "192.168.137.1",
+            ip: getIp(),
+            // announcedIp: process.env.HOST_PUBLIC_IP ?? "192.168.137.1",
           },
-          ...returnIps()
+          // ...returnIps()
         ],
         enableUdp: true,
         enableTcp: true,
